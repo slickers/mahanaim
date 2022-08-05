8bd38bce899ca7b7df90650b91e9bb5a69248646
slickers <slickers@users.noreply.github.com>
2022-08-06 00:07:29 +0700

    Update destroy_jemaat.php

--- apps/manage/destroy_jemaat.php
+++ apps/manage/destroy_jemaat.php
@@ -1,32 +1,15 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "Rahasia123";
-$dbname = "cms_db";
-
-// Create connection
-$conn = mysqli_connect($servername, $username, $password, $dbname);
-// Check connection
-if (!$conn) {
-    die("Connection failed: " . mysqli_connect_error());
-}
+include('../../conf/config.php');
 
 $id_jmt = $_GET['id_jmt'];
-
-// $query = mysqli_query(
-//     $koneksi,
-//     "INSERT INTO jemaat(no,nama,tp_lahir,tg_lahir,gender,gol_darah,pendidikan,pekerjaan,member_keluarga,posisi_keluarga,status_baptis,status_sidi,status_nikah,kwp,pelka,status_jemaat) 
-//     VALUES ('',$nama,$pob,$dob,$gender,$darah,$didik,$kerja,$member,$posisi,$baptis,$sidi,$nikah,$kwp,$pelka,$status)"
-// );
-// header('Location: ../index.php?page=edit-jemaat');
 
 $sql = "DELETE FROM tbl_jemaat WHERE id_jmt='$id_jmt'";
 
 
-if (mysqli_query($conn, $sql)) {
+if (mysqli_query($koneksi, $sql)) {
     header('Location: ../index.php?page=jemaat');
 } else {
     header('Location: ../index.php?page=errpage');
 }
 
-mysqli_close($conn);
+mysqli_close($koneksi);
