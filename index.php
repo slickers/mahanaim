1b557d77b73fd1063f37e5948590bd43024c8303
slickers <slickers@users.noreply.github.com>
2022-08-06 00:22:17 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,3 @@
 <?php
-//membuat metode redirect dengan kode 301
 header('location: viewonly.php', true, 301);
-//membuat kode di bawah header tidak diproses oleh website sehingga lebih aman
 exit();
