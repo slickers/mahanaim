dc458080866f9fb1789e5d1b3e4448a2a78469df
slickers <slickers@users.noreply.github.com>
2022-08-06 00:08:21 +0700

    Update destroy_keluarga.php

--- apps/manage/destroy_keluarga.php
+++ apps/manage/destroy_keluarga.php
@@ -3,13 +3,6 @@ include('../../conf/config.php');
 
 $id_kel = $_GET['id_kel'];
 
-// $query = mysqli_query(
-//     $koneksi,
-//     "INSERT INTO jemaat(no,nama,tp_lahir,tg_lahir,gender,gol_darah,pendidikan,pekerjaan,member_keluarga,posisi_keluarga,status_baptis,status_sidi,status_nikah,kwp,pelka,status_jemaat) 
-//     VALUES ('',$nama,$pob,$dob,$gender,$darah,$didik,$kerja,$member,$posisi,$baptis,$sidi,$nikah,$kwp,$pelka,$status)"
-// );
-// header('Location: ../index.php?page=edit-jemaat');
-
 $sql = "DELETE FROM tbl_keluarga WHERE id_kel='$id_kel'";
 
 if (mysqli_query($koneksi, $sql)) {
