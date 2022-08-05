0371d4cf18d4898ddc08eab6b8057b383e596c79
slickers <slickers@users.noreply.github.com>
2022-08-06 00:13:45 +0700

    Update tambah_keluarga.php

--- apps/manage/tambah_keluarga.php
+++ apps/manage/tambah_keluarga.php
@@ -1,15 +1,5 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "Rahasia123";
-$dbname = "cms_db";
-
-// Create connection
-$conn = mysqli_connect($servername, $username, $password, $dbname);
-// Check connection
-if (!$conn) {
-  die("Connection failed: " . mysqli_connect_error());
-}
+include('../../conf/config.php');
 
 $nama_kel = $_GET['nama_kel'];
 $kwp = $_GET['kwp'];
@@ -22,21 +12,13 @@ $telepon = $_GET['no_telepon'];
 $stat_kel = $_GET['stat_kel'];
 $info_kel = $_GET['info_kel'];
 
-
-// $query = mysqli_query(
-//     $koneksi,
-//     "INSERT INTO jemaat(no,nama,tp_lahir,tg_lahir,gender,gol_darah,pendidikan,pekerjaan,member_keluarga,posisi_keluarga,status_baptis,status_sidi,status_nikah,kwp,pelka,status_jemaat) 
-//     VALUES ('',$nama,$pob,$dob,$gender,$darah,$didik,$kerja,$member,$posisi,$baptis,$sidi,$nikah,$kwp,$pelka,$status)"
-// );
-// header('Location: ../index.php?page=edit-jemaat');
-
 $sql = "INSERT INTO tbl_keluarga (nama_kel, kwp, kepala, tglnikah, alamat, propinsi, kodepos, telepon, stat_kel, info_kel)
 VALUES ('$nama_kel','$kwp','$kepala','$tglnikah','$alamat','$propinsi','$kodepos','$telepon','$stat_kel','$info_kel')";
 
-if (mysqli_query($conn, $sql)) {
+if (mysqli_query($koneksi, $sql)) {
   header('Location: ../index.php?page=keluarga');
 } else {
   header('Location: ../index.php?page=errpage');
 }
 
-mysqli_close($conn);
+mysqli_close($koneksi);
