aed17c6fdc2acdff9b8a701ea56a35589aabc90e
slickers <slickers@users.noreply.github.com>
2022-08-05 17:11:54 +0700

    Add files via upload

---
+++ apps/dist/js/chart.js
@@ -0,0 +1,28 @@
+$(function () {
+  $.ajax({
+    url: "pages/chart_data.php",
+    type: "GET",
+    success: function (data) {
+      chartData = data;
+      var chartProperties = {
+        caption: "Rasio Usia",
+        xAxisName: "kelompok",
+        yAxisName: "jumlah",
+        rotatevalues: "0",
+        theme: "zune",
+      };
+      apiChart = new FusionCharts({
+        type: "column2d",
+        renderAt: "chart-container",
+        width: "800",
+        height: "400",
+        dataFormat: "json",
+        dataSource: {
+          chart: chartProperties,
+          data: chartData,
+        },
+      });
+      apiChart.render();
+    },
+  });
+});
