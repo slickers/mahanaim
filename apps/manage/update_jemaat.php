4fb1e3a3ae18df1a3f6da8d0b18ee5ee0c3edeaf
slickers <slickers@users.noreply.github.com>
2022-08-06 00:15:49 +0700

    Update update_jemaat.php

--- apps/manage/update_jemaat.php
+++ apps/manage/update_jemaat.php
@@ -1,15 +1,5 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "Rahasia123";
-$dbname = "cms_db";
-
-// Create connection
-$conn = mysqli_connect($servername, $username, $password, $dbname);
-// Check connection
-if (!$conn) {
-    die("Connection failed: " . mysqli_connect_error());
-}
+include('../../conf/config.php');
 
 $id_jmt = $_GET['id_jmt'];
 $nama = $_GET['nama'];
@@ -29,19 +19,12 @@ $nikah = $_GET['menikah'];
 $pelka = $_GET['pelka'];
 $status = $_GET['stat_jmt'];
 
-// $query = mysqli_query(
-//     $koneksi,
-//     "INSERT INTO jemaat(no,nama,tp_lahir,tg_lahir,gender,gol_darah,pendidikan,pekerjaan,member_keluarga,posisi_keluarga,status_baptis,status_sidi,status_nikah,kwp,pelka,status_jemaat) 
-//     VALUES ('',$nama,$pob,$dob,$gender,$darah,$didik,$kerja,$member,$posisi,$baptis,$sidi,$nikah,$kwp,$pelka,$status)"
-// );
-// header('Location: ../index.php?page=edit-jemaat');
-
 $sql = "UPDATE tbl_jemaat SET nama='$nama', tplahir='$pob', tglahir='$dob', gender='$gender', goldarah='$darah', pendidikan='$didik', pekerjaan='$kerja', keluarga='$member', posisi='$posisi', tpbaptis='$tpbaptis', tglbaptis='$tglbaptis', tpsidi='$tpsidi', tglsidi='$tglsidi', menikah='$nikah', pelka='$pelka', stat_jmt='$status' WHERE id_jmt='$id_jmt'";
 
-if (mysqli_query($conn, $sql)) {
+if (mysqli_query($koneksi, $sql)) {
     header('Location: ../index.php?page=jemaat');
 } else {
     header('Location: ../index.php?page=errpage');
 }
 
-mysqli_close($conn);
+mysqli_close($koneksi);
