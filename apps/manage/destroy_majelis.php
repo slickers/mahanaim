a1d21dd97500c3857e9173626b979ce0ad112645
slickers <slickers@users.noreply.github.com>
2022-08-06 00:09:02 +0700

    Update destroy_majelis.php

--- apps/manage/destroy_majelis.php
+++ apps/manage/destroy_majelis.php
@@ -1,31 +1,14 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "Rahasia123";
-$dbname = "cms_db";
-
-// Create connection
-$conn = mysqli_connect($servername, $username, $password, $dbname);
-// Check connection
-if (!$conn) {
-    die("Connection failed: " . mysqli_connect_error());
-}
+include('../../conf/config.php');
 
 $id_mjl = $_GET['id_mjl'];
 
-// $query = mysqli_query(
-//     $koneksi,
-//     "INSERT INTO jemaat(no,nama,tp_lahir,tg_lahir,gender,gol_darah,pendidikan,pekerjaan,member_keluarga,posisi_keluarga,status_baptis,status_sidi,status_nikah,kwp,pelka,status_jemaat) 
-//     VALUES ('',$nama,$pob,$dob,$gender,$darah,$didik,$kerja,$member,$posisi,$baptis,$sidi,$nikah,$kwp,$pelka,$status)"
-// );
-// header('Location: ../index.php?page=edit-jemaat');
-
 $sql = "DELETE FROM tbl_majelis WHERE id_mjl='$id_mjl'";
 
-if (mysqli_query($conn, $sql)) {
+if (mysqli_query($koneksi, $sql)) {
     header('Location: ../index.php?page=majelis');
 } else {
     header('Location: ../index.php?page=errpage');
 }
 
-mysqli_close($conn);
+mysqli_close($koneksi);
