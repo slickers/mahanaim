07b62a5375d9f225a25bae0af37394d538c8da9b
slickers <slickers@users.noreply.github.com>
2022-08-09 22:26:36 +0700

    Update config.php

--- conf/config.php
+++ conf/config.php
@@ -1,5 +1,5 @@
 <?php
-$koneksi = mysqli_connect('remotemysql.com', 'ipn5W0Uiqc', 'O0lRBL8H7Z', 'cms_db');
+$koneksi = mysqli_connect('remotemysql.com', 'ipn5W0Uiqc', 'O0lRBL8H7Z', 'ipn5W0Uiqc');
 
 if (!$koneksi) {
     die("Koneksi Gagal:" . mysqli_connect_error());
